da1b9f2652203c0fa8afc0957761181d1b56161b
Luke Patrick <luke@snodandjeff.com>
2015-08-03 18:51:04 +0000

    Adds new middle section, stages for widgetizing

--- home.php
+++ home.php
@@ -4,6 +4,7 @@
 get_header();
 ?>
 
+<?php //Let's add our Hero section ?>
 
 <section class="hero">
   
@@ -35,6 +36,75 @@ get_header();
   </div>
   
 </section>
+
+<?php //Now, let's throw in our middle text & photo section ?>
+
+<section class="home-middle">
+  
+  <div class="row middle">
+    <h2>This is the Title Block</h2>
+    <p>Add anything you want. It can go here, it can go there, it can go just about anywhere. No worries, whatever you want! Just put it in this text block.</p>
+  </div>
+  
+  <?php //Add the fancy cards section that we're going to use for more info ?>
+  <div class="row cards">
+    
+    <?php //First card ?>
+  <div class="large-4 columns">
+
+    <div class="image-wrapper overlay-slide-in-left">
+      
+      <img src="/wp-content/themes/squarefoot/assets/img/move-in.jpg" />
+      
+      <div class="image-overlay-content">
+        <h2>View Floorplans</h2>
+        <p class="price">move-in specials</p>
+        <a href="#" class="button">See Now</a>
+      </div>
+    
+    </div>
+  
+    </div>
+    
+    <?php //Second Card ?>
+    <div class="large-4 columns">
+  
+      <div class="image-wrapper overlay-slide-in-left">
+        
+        <img src="/wp-content/themes/squarefoot/assets/img/amens.jpg" />
+        
+        <div class="image-overlay-content">
+          <h2>World-Class Amenities</h2>
+          <p class="price">all the works</p>
+          <a href="#" class="button">See Now</a>
+        </div>
+      
+      </div>
+    
+    </div> 
+    
+    <?php //Third Card ?>
+    <div class="large-4 columns">
+  
+      <div class="image-wrapper overlay-slide-in-left">
+        
+        <img src="/wp-content/themes/squarefoot/assets/img/neighborhood.jpg" />
+        
+        <div class="image-overlay-content">
+          <h2>The Neighborhood</h2>
+          <p class="price">a prime location</p>
+          <a href="#" class="button">Want!</a>
+        </div>
+      
+      </div>
+    
+    </div>
+      <?php //End cards ?>
+  </div>
+
+  
+</section>
+
 
 
 <?php
