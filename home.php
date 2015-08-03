5bcb4d875fbcbc73964b1fafbb81453ca643f332
Luke Patrick <luke@snodandjeff.com>
2015-08-03 02:34:49 +0000

    Adds new Hero section, primes for enabling widget registration

---
+++ home.php
@@ -0,0 +1,23 @@
+<?php
+//Lets add our typical WordPress stuff, then fill it all in with some bad-ass layouts inspired by the awesome building blocks Zurb provides
+
+get_header();
+?>
+
+
+<section class="hero">
+  <div class="row intro">
+    <div class="small-centered medium-uncentered medium-6 large-7 columns">
+      <h1>Your New Home <br> Just Around the Corner</h1>
+      <p>The apartments at Squarefoot are centrally located, fairly priced, and endlessly comfortable. Finding your new home has never been so easy.</p>
+    </div>
+    <div class="small-centered medium-uncentered medium-6 large-5 columns">
+      <div class="tech-img"></div>
+    </div>
+  </div>
+</section>
+
+
+<?php
+get_footer();
+?>
