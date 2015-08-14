0f641c34293d74bb07dad5692e342b9dac6650e8
Luke Patrick <luke@snodandjeff.com>
2015-08-14 17:38:23 +0000

    Adds initial, unstyled edition of the working slider for the floor plans page, as well as hooks to grab from our custom post type and metadata. Truly snazzy stuff

--- templates/floor-plans.php
+++ templates/floor-plans.php
@@ -3,39 +3,86 @@
 Template Name: Floor Plans
 */
 
-/*
-Usage: floor_plan_details_get_meta( 'floor_plan_details_price' )
-	Usage: floor_plan_details_get_meta( 'floor_plan_details_squarefeet' )
-	Usage: floor_plan_details_get_meta( 'floor_plan_details_bed' )
-	Usage: floor_plan_details_get_meta( 'floor_plan_details_bath' ) 
-*/
-
 get_header(); ?>
 <div class="row">
 	<div class="small-12 large-12 columns" role="main">
-
-	<div class="slider-for">
-		<div><p>Test text here</p></div>
-		<div><p>Test text here</p></div>
-		<div><p>Test text here</p></div>
-		<div><p>Test text here</p></div>
-		<div><p>Test text here</p></div>
-	</div>
+	<?php while ( have_posts() ) : the_post(); ?>
+	<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+		<header>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</header>
+		
+		<?php
+		// Let's start our custom loop set for the slider and vertical nav
+		
+		// The args
+		$args = array (
+			'post_type'              => array( 'floor_plans' ),
+			'order'                  => 'DESC',
+			'orderby'                => 'date',
+		);
+		
+		// The Query
+		$floorplan_query = new WP_Query( $args );
+		?>
+		<div class="slider-nav">
+		<?php
+			// The Loop for Navigation
+			if ( $floorplan_query->have_posts() ) {
+				while ( $floorplan_query->have_posts() ) {
+					$floorplan_query->the_post();
+				?>
+					<div class="article">
+						<h4><?php the_title(); ?></h4>
+						<?php echo the_post_thumbnail('medium'); ?>
+					</div>
+				<?php
+				}
+			} else {
+				echo '<h3>I am afraid, Dave</h3>';
+			}
+			?>
+		</div>
+		
+		<?php
+		// Reset the post data before we do our second loop for actual content
+		wp_reset_postdata();
+		?>
+		
+		<div class="slider-for">
+			<?php
+			// The Loop for Content
+				if ( $floorplan_query->have_posts() ) {
+					while ( $floorplan_query->have_posts() ) {
+						$floorplan_query->the_post();
+					?>
+						<div class="article">
+							<div class="large-8 columns">
+								<h4><?php the_title(); ?></h4>
+								<p><?php the_content(); ?></p>
+								<div class="floorplan-info">
+									<p><span class="info-header">Price: </span><?php echo floor_plan_details_get_meta( "floor_plan_details_price" ); ?></p>
+									<p><span class="info-header">Squarefeet: </span><?php echo floor_plan_details_get_meta( "floor_plan_details_squarefeet" ); ?></p>
+									<p><span class="info-header">Bedrooms: </span><?php echo floor_plan_details_get_meta( "floor_plan_details_bed" ); ?></p>
+									<p><span class="info-header">Bathrooms: </span><?php echo floor_plan_details_get_meta( "floor_plan_details_bath" ); ?></p>
+								</div>
+							</div>
+							<div class="large-4 columns">
+								<?php echo the_post_thumbnail('medium'); ?>
+							</div>
+						</div>
+					<?php
+					}
+				} else {
+					echo '<h3>I am afraid, Dave</h3>';
+				}
+			?>
+		</div>
+			
+		</article> 
+	<?php endwhile; // End the loop ?>
 	
-	<div class="slider-nav">
-		<div><p>Test text here</p></div>
-		<div><p>Test text here</p></div>
-		<div><p>Test text here</p></div>
-		<div><p>Test text here</p></div>
-		<div><p>Test text here</p></div>
-	</div>
-
 	</div>
 </div>
-
-
-<script type="text/javascript">
-
-</script>
 
 <?php get_footer(); ?>
